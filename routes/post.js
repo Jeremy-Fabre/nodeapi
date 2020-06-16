9b46104800ed6f011d9ce8ca25a13d7a452ffadf
Jérémy Fabre <jef@MacBook-Pro-de-Jeremy.local>
2020-06-16 06:06:01 +0200

    deploy

---
+++ routes/post.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const {
+    getPosts, 
+    createPost, 
+    postsByUser, 
+    postById, 
+    isPoster, 
+    updatePost, 
+    deletePost,
+    photo,
+    singlePost,
+    like,
+    unlike,
+    comment,
+    uncomment
+    } = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const {createPostValidator} = require('../validator');
+
+const router = express.Router();
+
+// Like & Unlike
+router.put("/post/like", requireSignin, like);
+router.put("/post/unlike", requireSignin, unlike);
+
+// Comments
+router.put("/post/comment", requireSignin, comment);
+router.put("/post/uncomment", requireSignin, uncomment);
+
+// Post routes
+router.get('/posts', getPosts);
+router.post(
+    '/post/new/:userId', 
+    requireSignin, 
+    createPost, 
+    createPostValidator
+);
+router.get("/posts/by/:userId", requireSignin,postsByUser);
+router.get('/post/:postId', singlePost)
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+router.get("/post/photo/:postId", photo);
+
+
+// Any route containing :userId, our app will first execute userByID()
+router.param("userId", userById);
+// Any route containing :userId, our app will first execute postByID()
+router.param("postId", postById);
+
+module.exports = router;
